e78aff5c35700699abe64cda1a4349297687a2cc
in-in <0866222@gmail.com>
2017-04-23 09:31:27 +0300

    add browser-sync

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,4 +1,6 @@
 import gulp from 'gulp';
+import del from 'del';
+import browserSync from 'browser-sync';
 import mjml from 'gulp-mjml';
 
 const paths = {
@@ -6,16 +8,25 @@ const paths = {
   src: 'src/',
 };
 
-// const defaultTask = done => {
-//   console.log('foobar');
-//   done();
-// };
+const server = browserSync.create();
 
-// function build () {
-//   return gulp.src(paths.src + '*.mjml')
-//     .pipe(mjml())
-//     .pipe(gulp.dest(paths.dist))
-// }
+function reload(done) {
+  server.reload();
+  done();
+}
+
+function serve(done) {
+  server.init({
+    server: { baseDir: paths.dist },
+    ui: false,
+    open: false,
+    reloadOnRestart: true,
+    notify: false
+  });
+  done();
+}
+
+const clean = () => del([paths.dist]);
 
 const build = () => {
   return gulp.src(paths.src + '*.mjml')
@@ -23,7 +34,8 @@ const build = () => {
     .pipe(gulp.dest(paths.dist))
 };
 
+const watch = () => gulp.watch(paths.src, gulp.series(build, reload));
 
-// gulp.task('default', build);
+const dev = gulp.series(clean, build, serve, watch);
 
-export default build;
+export default dev;
